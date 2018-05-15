621a38f6efb74a4730587f632b175e23cd60b923
MeandrousShark <mphannon29@gmail.com>
2018-05-15 15:28:03 -0700

    we are now doubly active with a module menu

--- app/src/main/java/com/aslearn/MainActivity.java
+++ app/src/main/java/com/aslearn/MainActivity.java
@@ -1,7 +1,9 @@
 package com.aslearn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void moduleMenu(View view) {
+        System.out.println("moduleMenu button clicked!");
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
     }
 }
