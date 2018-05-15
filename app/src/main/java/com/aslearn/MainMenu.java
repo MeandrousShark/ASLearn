40b09f568c05392988c7b85fdf5176edb1515307
MeandrousShark <mphannon29@gmail.com>
2018-05-15 15:35:37 -0700

    we can now return to our parents

--- app/src/main/java/com/aslearn/MainMenu.java
+++ app/src/main/java/com/aslearn/MainMenu.java
@@ -7,6 +7,6 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.module_menu);
     }
 }
