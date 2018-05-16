ece72b743381136b5f09ead96ef6836e54355f2f
MeandrousShark <mphannon29@gmail.com>
2018-05-15 17:37:41 -0700

    started Created framework for opening the lesson screens

--- app/src/main/java/com/aslearn/MainMenu.java
+++ app/src/main/java/com/aslearn/MainMenu.java
@@ -1,12 +1,25 @@
 package com.aslearn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.module_menu);
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void greetings(View view) {
+        //@TODO Figure out the lesson view (where all the signs in the lesson are)
+        // Intent intent = new Intent(this, MainMenu.class);
+        // startActivity(intent);
     }
 }
