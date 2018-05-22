798b66e0504cd76e4be702c7e0ecbdb9c43fb496
vancoul <vancoul@students.wwu.edu>
2018-05-21 22:27:13 -0700

    Added getters and setters to Word entity

--- app/src/main/java/com/aslearn/db/Word.java
+++ app/src/main/java/com/aslearn/db/Word.java
@@ -6,7 +6,7 @@ import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.PrimaryKey;
 
 @Entity(tableName = "words", foreignKeys = @ForeignKey(entity = Lesson.class,
-                                                        parentColumns = "lesson_id",
+                                                        parentColumns = "lessonName",
                                                         childColumns = "lesson"))
 public class Word {
     @PrimaryKey (autoGenerate = true)
@@ -16,15 +16,75 @@ public class Word {
     private String word;
 
     @ColumnInfo (name="visual_file")
-    private String visual_file;
+    private String visualFile;
 
     @ColumnInfo (name = "basic_info")
-    private String basic_info;
+    private String basicInfo;
 
     @ColumnInfo (name = "more_info")
-    private String more_info;
+    private String moreInfo;
 
     @ColumnInfo (name="lesson")
-    private int lesson;
+    private String lesson;
 
+    private int fluencyVal;
+
+    //Constructors
+    private Word (String word, String visualFile, String basicInfo, String moreInfo, String lesson){
+        this.word=word;
+        this.visualFile=visualFile;
+        this.basicInfo=basicInfo;
+        this.moreInfo=moreInfo;
+        this.lesson=lesson;
+        this.fluencyVal=0;
+    }
+
+    //Getters and Setters
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getVisualFile() {
+        return visualFile;
+    }
+
+    public void setVisualFile(String visualFile) {
+        this.visualFile = visualFile;
+    }
+
+    public String getBasicInfo() {
+        return basicInfo;
+    }
+
+    public void setBasicInfo(String basicInfo) {
+        this.basicInfo = basicInfo;
+    }
+
+    public String getMoreInfo() {
+        return moreInfo;
+    }
+
+    public void setMoreInfo(String moreInfo) {
+        this.moreInfo = moreInfo;
+    }
+
+    public String getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(String lesson) {
+        this.lesson = lesson;
+    }
+
+    public int getFluencyVal() {
+        return fluencyVal;
+    }
+
+    public void setFluencyVal(int fluencyVal) {
+        this.fluencyVal = fluencyVal;
+    }
 }
