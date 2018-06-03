705c0b6a68f125457887b8ad3e297fe0fec38c99
vancoul <0carina!>
2018-06-03 13:13:56 -0700

    made quiz activity class but it doesn't do anything.

---
+++ app/src/main/java/com/aslearn/Quiz.java
@@ -0,0 +1,16 @@
+package com.aslearn;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.aslearn.db.Question;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vancoul on 6/3/18.
+ */
+
+public class Quiz extends AppCompatActivity {
+    ArrayList<Question> questions;
+
+}
