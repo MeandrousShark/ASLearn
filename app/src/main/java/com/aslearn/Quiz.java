fdce398148efe349c9b21b309b070c8be31e00e9
MeandrousShark <mphannon29@gmail.com>
2018-06-03 14:03:46 -0700

    started quiz class

--- app/src/main/java/com/aslearn/Quiz.java
+++ app/src/main/java/com/aslearn/Quiz.java
@@ -1,6 +1,8 @@
 package com.aslearn;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.aslearn.db.Question;
 
@@ -12,5 +14,31 @@ import java.util.ArrayList;
 
 public class Quiz extends AppCompatActivity {
     ArrayList<Question> questions;
+    private Question question;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    private void setupQuestion() {
+        switch(question.getType()) {
+            case "sign2eng":
+              //  setContentView(R.layout.multiple_choice);
+                break;
+            case "eng2sign":
+                //TODO make multiple choice
+              //  setContentView(R.layout.multiple_choice);
+                break;
+            case "textEntry":
+              //  setContentView(R.layout.fingerspelling_questions);
+                break;
+            case "matching":
+                //TODO make matching
+        //        setContentView(R.layout.matching);
+                break;
+        }
+
+    }
 }
