1e2c2992485efd8b3eda676adc659618fb4fa994
MeandrousShark <mphannon29@gmail.com>
2018-06-08 16:15:37 -0700

    Dictionary set up, sign loading is in progress

--- app/src/main/java/com/aslearn/Dictionary.java
+++ app/src/main/java/com/aslearn/Dictionary.java
@@ -1,8 +1,11 @@
 package com.aslearn;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
@@ -25,15 +28,35 @@ public class Dictionary extends AppCompatActivity {
         setContentView(R.layout.dictionary);
     }
 
-    public void searchDictionary(View view){
+    public void searchDictionary(View view) {
+        //TODO Bug: When Entering multiple inputs to the dictionary search, it will just keep adding things
         EditText searchInput = findViewById(R.id.searchInput);
         String searchWord = searchInput.getText().toString();
-        ArrayList<Word> resultList = dbAccess.selectWord(searchWord);
-        LinearLayout layout = findViewById(R.id.linearLayout);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(30, 20, 30, 20);
-        layoutParams.height = 250;
-
+        final ArrayList<Word> resultList = dbAccess.selectWord(searchWord);
+        System.out.println(resultList.get(0));
+        Button[] resultButtons = new Button[resultList.size()];
+        final LinearLayout layout = findViewById(R.id.DictionaryEntries);
+        for (int i = 0; i < resultButtons.length; i++) {
+            final int I = i;
+            Button resultButton;
+            resultButton = new Button(this);
+            resultButton.setBackground(getDrawable(R.drawable.buttonrounding));
+            // lessonButton.setBackgroundColor(0xFFFFFE);
+            resultButton.setText(resultList.get(i).getWord());
+            resultButton.setTextSize(24);
+            resultButton.setTextColor(Color.GRAY);
+            resultButton.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            resultButton.setPadding(90, 0, 90, 0);
+            layout.addView(resultButton);
+            resultButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    layout.removeAllViewsInLayout();
+                    Intent intent = new Intent(Dictionary.this, SignDictionary.class);
+                    intent.putExtra("wordName", resultList.get(I).getWord());
+                    startActivity(intent);
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/com/aslearn/SignDictionary.java
@@ -0,0 +1,17 @@
+package com.aslearn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hannonm2 on 6/8/18.
+ */
+
+public class SignDictionary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dictionary_sign);
+    }
+}
